6572f1bb5eaa2c8be2c6b05cfff4a5cc37062f6f
unknown <yunhanmin@naver.comgit config --global user.name Hanmin>
2015-11-17 11:19:38 +0900

    Superclasses and Sorting methods

---
+++ SortArray.java
@@ -0,0 +1,35 @@
+// SortArray.java
+
+public class SortArray {
+  
+  // instance variables
+  public int[] array = new int[12];
+  
+  // constructor
+  public SortArray() {
+    array = new int[]{ 76, 71, 63, 65, 66, 64, 70, 69, 68, 67, 72, 73 };
+  }
+  
+  // methods
+  public String toString() {
+    
+    String result = "";
+    
+    result += "{ ";
+    for( int i = 0; i < array.length-1; i++ ) {
+      result = result + array[i] + ", ";
+    }
+    result = result + array[array.length-1] + " }";
+    
+    return result;
+    
+  } // END toString
+  
+  public void swap( int first, int second ) {
+    System.out.println("swapping");
+    int temp = array[first];
+    array[first] = array[second];
+    array[second] = temp;
+  }
+  
+} // END class
---
+++ Sorter.java
@@ -0,0 +1,30 @@
+/*
+ * Project: Sorter.java
+ * Description: Main class that implements many different sorting methods from Sort classes
+ * Name: Aaron Snowberger
+ * Date: Nov 17, 2015
+ */
+
+public class Sorter {
+  
+  public static void main( String[] args ) {
+    
+    // Bubble Sort
+    BubbleArray newone = new BubbleArray();
+    System.out.println( "Bubble unsorted: " + newone );
+    newone.bubbleSort();
+    System.out.println( "Bubble sorted: " + newone );
+    
+    // Selection Sort
+    
+    // Insertion Sort
+    
+    // Quick Sort
+    
+    // Merge Sort
+    
+    // Radix Sort
+    
+  } // END main method
+  
+} // END class Sorter
